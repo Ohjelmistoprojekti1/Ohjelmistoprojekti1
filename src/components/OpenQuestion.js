a5f0ab66db4896d7151cc1fe67bc31752d5a3b65
tuukkakotilainen <tuukka.kotilainen1@gmail.com>
2020-04-16 14:50:12 +0300

    Lisäsin avoimen kysymyksen ja tein vastausten tallentamisen radiokysymykseen ja avoimeen kysymykseen.

---
+++ src/components/OpenQuestion.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function OpenQuestion() {
+    const [answer, setAnswer] = React.useState('');
+    const [question, setQuestion] = React.useState('');
+
+    React.useEffect(() => {
+        fetch('') //Tähän tulee linkki herokuun
+        .then(response => response.json())
+        .then ((responseData) => {
+            setQuestion(responseData.results[0].question);
+        })
+    }, [])
+
+    const inputChanged = (event) => {
+        setAnswer(event.target.value);
+    }
+
+    const saveAnswer = (answer) => {
+        fetch('', //tähän tulee linkki herokuun
+            {
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(answer)
+            }
+        )
+
+    }
+       
+
+    return (
+        
+        <div>
+            <p>{question}Tähän tulee kysymys</p>
+            <input type="text" size="100" value={answer} onChange={inputChanged} />
+            <button onClick={saveAnswer}>Tallenna</button>
+        </div>
+    )
+}
