6d88db58a82bbf376f067ee74cdf01c76e2e2016
tuukkakotilainen <tuukka.kotilainen1@gmail.com>
2020-04-26 12:11:50 +0300

    Tein loginin

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './App.css';
 import Radio from './components/Radio.js';
 import OpenQuestion from './components/OpenQuestion';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Login/>
+        </Toolbar>
+      </AppBar>
+      <br/><br/><br/>
       <Radio />
       <br/><br/><br/>
       <OpenQuestion />
